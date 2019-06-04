c82335a399824c9cf526fc5889860b2af8372193
Negar <n.jamalifard@gmail.com>
2019-06-04 19:44:33 +0430

    add scale ui styles

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import "@/assets/scss/Style.scss";
+import "@/assets/scss/Style.scss";
 
 Vue.config.productionTip = false;
 
